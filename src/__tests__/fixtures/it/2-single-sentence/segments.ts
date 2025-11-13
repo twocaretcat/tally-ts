4c244dca032e039b078a7a3e1a6498339bbe46fa
John Goodliff <9030780+twocaretcat@users.noreply.github.com>
2025-11-13 00:06:39 -0700

    test: add fixtures for it

---
+++ src/__tests__/fixtures/it/2-single-sentence/segments.ts
@@ -0,0 +1,78 @@
+// AUTO-GENERATED — DO NOT EDIT
+// Run scripts/generate-segments.mjs to regenerate
+import type { SegmentsMap } from '../../../types.ts';
+
+const segmentsMap: SegmentsMap = {
+	"graphemes": [
+		{
+			"segment": "M",
+			"index": 0
+		},
+		{
+			"segment": "i",
+			"index": 1
+		},
+		{
+			"segment": " ",
+			"index": 2
+		},
+		{
+			"segment": "m",
+			"index": 3
+		},
+		{
+			"segment": "a",
+			"index": 4
+		},
+		{
+			"segment": "n",
+			"index": 5
+		},
+		{
+			"segment": "c",
+			"index": 6
+		},
+		{
+			"segment": "h",
+			"index": 7
+		},
+		{
+			"segment": "i",
+			"index": 8
+		},
+		{
+			"segment": ".",
+			"index": 9
+		}
+	],
+	"words": [
+		{
+			"segment": "Mi",
+			"index": 0,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 2,
+			"isWordLike": false
+		},
+		{
+			"segment": "manchi",
+			"index": 3,
+			"isWordLike": true
+		},
+		{
+			"segment": ".",
+			"index": 9,
+			"isWordLike": false
+		}
+	],
+	"sentences": [
+		{
+			"segment": "Mi manchi.",
+			"index": 0
+		}
+	]
+};
+
+export default segmentsMap;
---
+++ src/__tests__/fixtures/it/4-multiple-sentences/segments.ts
@@ -0,0 +1,325 @@
+// AUTO-GENERATED — DO NOT EDIT
+// Run scripts/generate-segments.mjs to regenerate
+import type { SegmentsMap } from '../../../types.ts';
+
+const segmentsMap: SegmentsMap = {
+	"graphemes": [
+		{
+			"segment": "N",
+			"index": 0
+		},
+		{
+			"segment": "o",
+			"index": 1
+		},
+		{
+			"segment": "n",
+			"index": 2
+		},
+		{
+			"segment": " ",
+			"index": 3
+		},
+		{
+			"segment": "p",
+			"index": 4
+		},
+		{
+			"segment": "o",
+			"index": 5
+		},
+		{
+			"segment": "s",
+			"index": 6
+		},
+		{
+			"segment": "s",
+			"index": 7
+		},
+		{
+			"segment": "o",
+			"index": 8
+		},
+		{
+			"segment": " ",
+			"index": 9
+		},
+		{
+			"segment": "d",
+			"index": 10
+		},
+		{
+			"segment": "i",
+			"index": 11
+		},
+		{
+			"segment": "r",
+			"index": 12
+		},
+		{
+			"segment": "g",
+			"index": 13
+		},
+		{
+			"segment": "l",
+			"index": 14
+		},
+		{
+			"segment": "i",
+			"index": 15
+		},
+		{
+			"segment": "e",
+			"index": 16
+		},
+		{
+			"segment": "l",
+			"index": 17
+		},
+		{
+			"segment": "o",
+			"index": 18
+		},
+		{
+			"segment": " ",
+			"index": 19
+		},
+		{
+			"segment": "a",
+			"index": 20
+		},
+		{
+			"segment": "d",
+			"index": 21
+		},
+		{
+			"segment": "e",
+			"index": 22
+		},
+		{
+			"segment": "s",
+			"index": 23
+		},
+		{
+			"segment": "s",
+			"index": 24
+		},
+		{
+			"segment": "o",
+			"index": 25
+		},
+		{
+			"segment": ".",
+			"index": 26
+		},
+		{
+			"segment": " ",
+			"index": 27
+		},
+		{
+			"segment": "N",
+			"index": 28
+		},
+		{
+			"segment": "o",
+			"index": 29
+		},
+		{
+			"segment": "n",
+			"index": 30
+		},
+		{
+			"segment": " ",
+			"index": 31
+		},
+		{
+			"segment": "è",
+			"index": 32
+		},
+		{
+			"segment": " ",
+			"index": 33
+		},
+		{
+			"segment": "p",
+			"index": 34
+		},
+		{
+			"segment": "i",
+			"index": 35
+		},
+		{
+			"segment": "ù",
+			"index": 36
+		},
+		{
+			"segment": " ",
+			"index": 37
+		},
+		{
+			"segment": "c",
+			"index": 38
+		},
+		{
+			"segment": "o",
+			"index": 39
+		},
+		{
+			"segment": "s",
+			"index": 40
+		},
+		{
+			"segment": "ì",
+			"index": 41
+		},
+		{
+			"segment": " ",
+			"index": 42
+		},
+		{
+			"segment": "s",
+			"index": 43
+		},
+		{
+			"segment": "e",
+			"index": 44
+		},
+		{
+			"segment": "m",
+			"index": 45
+		},
+		{
+			"segment": "p",
+			"index": 46
+		},
+		{
+			"segment": "l",
+			"index": 47
+		},
+		{
+			"segment": "i",
+			"index": 48
+		},
+		{
+			"segment": "c",
+			"index": 49
+		},
+		{
+			"segment": "e",
+			"index": 50
+		},
+		{
+			"segment": ".",
+			"index": 51
+		}
+	],
+	"words": [
+		{
+			"segment": "Non",
+			"index": 0,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 3,
+			"isWordLike": false
+		},
+		{
+			"segment": "posso",
+			"index": 4,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 9,
+			"isWordLike": false
+		},
+		{
+			"segment": "dirglielo",
+			"index": 10,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 19,
+			"isWordLike": false
+		},
+		{
+			"segment": "adesso",
+			"index": 20,
+			"isWordLike": true
+		},
+		{
+			"segment": ".",
+			"index": 26,
+			"isWordLike": false
+		},
+		{
+			"segment": " ",
+			"index": 27,
+			"isWordLike": false
+		},
+		{
+			"segment": "Non",
+			"index": 28,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 31,
+			"isWordLike": false
+		},
+		{
+			"segment": "è",
+			"index": 32,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 33,
+			"isWordLike": false
+		},
+		{
+			"segment": "più",
+			"index": 34,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 37,
+			"isWordLike": false
+		},
+		{
+			"segment": "così",
+			"index": 38,
+			"isWordLike": true
+		},
+		{
+			"segment": " ",
+			"index": 42,
+			"isWordLike": false
+		},
+		{
+			"segment": "semplice",
+			"index": 43,
+			"isWordLike": true
+		},
+		{
+			"segment": ".",
+			"index": 51,
+			"isWordLike": false
+		}
+	],
+	"sentences": [
+		{
+			"segment": "Non posso dirglielo adesso. ",
+			"index": 0
+		},
+		{
+			"segment": "Non è più così semplice.",
+			"index": 28
+		}
+	]
+};
+
+export default segmentsMap;
